205ffe9911d3531364ee734d64094fff64b76c7a
yiminliao <yiminliao@aten.com.tw>
2023-04-27 17:53:06 +0800

    what:[feat] Update readme
    1. Update readme and show how to use
    2. Modify ConverterStatus to get status name

--- enums.js
+++ enums.js
@@ -1,3 +1,10 @@
+class Status {
+    constructor(name, value) {
+        this.name = name;
+        this.value = value;
+    }
+}
+
 const Voice = {
     NOETIC: 'Aurora_noetic',
     LITERARY: 'Aaron_literary',
@@ -8,14 +15,14 @@ const Voice = {
 }
 
 const ConverterStatus = {
-    ConverterStartUp: 0,
-    ConverVoiceStart: 10,
-    ConverVoiceRunning: 11,
-    ConverVoiceCompleted: 12,
-    ConverVoiceFail: 13,
-    ServerBusy: 21,
-    GetSpeechSuccess: 91,
-    GetSpeechFail: 92
+    ConverterStartUp: new Status("ConverterStartUp", 0),
+    ConverVoiceStart: new Status("ConverVoiceStart", 10),
+    ConverVoiceRunning: new Status("ConverVoiceRunning", 11),
+    ConverVoiceCompleted: new Status("ConverVoiceCompleted", 12),
+    ConverVoiceFail: new Status("ConverVoiceFail", 13),
+    ServerBusy: new Status("ServerBusy", 21),
+    GetSpeechSuccess: new Status("GetSpeechSuccess", 91),
+    GetSpeechFail: new Status("GetSpeechFail", 92)
 }
 
 module.exports = {
